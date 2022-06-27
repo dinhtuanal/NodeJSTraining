c8255fab7a1f756cc237cc64002071ca98e5cc17
Đình Tuân <94849754+dinhtuanal@users.noreply.github.com>
2022-06-27 16:18:18 +0700

    ...

--- src/routes/student/index.ts
+++ src/routes/student/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 const route = express.Router()
-import studentController from '../../app/controllers/studentController'
+import studentController from '../../app/controllers/StudentController'
 
 route.get('/students/',studentController.getAll)
 route.post('/students/',studentController.add)
