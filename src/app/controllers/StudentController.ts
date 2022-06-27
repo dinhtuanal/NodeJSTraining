0c05ed6e6efa27a89775e941a7053009c42e4f47
Đình Tuân <94849754+dinhtuanal@users.noreply.github.com>
2022-06-27 16:57:29 +0700

    Delete student

--- src/app/controllers/StudentController.ts
+++ src/app/controllers/StudentController.ts
@@ -1,34 +1,63 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import Student from "../models/Student";
 
-export default new class StudentController{
-    getAll = async (req:Request, res:Response, next:NextFunction)=>{
+export default new class StudentController {
+    getAll = async (req: Request, res: Response) => {
         const listStudent = await Student.find()
-        res.json({
-            data: listStudent
-        })
+        res.send(listStudent)
     }
 
-    add = async(req:Request, res:Response)=>{
+    add = async (req: Request, res: Response) => {
         await Student.create(req.body)
         res.status(200).json({
             success: true,
-            notice: 'Thêm mới thành công'
+            message: 'Thêm mới thành công'
         })
     }
 
-    getById = async (req:Request, res:Response, next:NextFunction )=>{
+    getById = async (req: Request, res: Response) => {
         const std = await Student.findById(req.params.id);
-        if(!std){
+        if (!std) {
             res.status(404).json({
                 success: false,
-                notice: 'Can not find student'
+                message: 'Can not find student'
             })
-        }else{
+        } else {
             res.status(200).json({
                 success: true,
                 data: std
             })
         }
     }
+
+    update = async (req: Request, res: Response) => {
+        const std = await Student.findById(req.body._id)
+        console.log(req.body._id)
+        if (!std) {
+            res.status(404).json({
+                success: false,
+                message: 'Can not find student'
+            })
+        } else {
+            //Chưa xử lý được nó lỗi quá nhiều
+        }
+    }
+
+    delete = async (req: Request, res: Response) => {
+        const std = await Student.findById(req.params.id)
+        if (!std) {
+            res.status(404).json({
+                success: false,
+                message: "Can not find student id"
+            })
+        }
+        else {
+            await Student.deleteOne({ _id: req.params.id })
+            res.status(200).json({
+                success: true,
+                message: "Delete success"
+            })
+        }
+    }
+
 }
--- src/routes/student/index.ts
+++ src/routes/student/index.ts
@@ -5,5 +5,7 @@ import studentController from '../../app/controllers/StudentController'
 route.get('/students/',studentController.getAll)
 route.post('/students/',studentController.add)
 route.get('/students/:id',studentController.getById)
+route.put('/students/',studentController.update)
+route.delete('/students/:id',studentController.delete)
 
 export default route
